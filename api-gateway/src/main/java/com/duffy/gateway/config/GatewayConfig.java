503379c3ec1303b50fffd30b1547d88b875bd96b
zzunho9 <jamlove4763@naver.com>
2026-02-18 21:47:09 +0900

    feat: Add static load balancing for consumer-api

---
+++ api-gateway/src/main/java/com/duffy/gateway/config/GatewayConfig.java
@@ -0,0 +1,18 @@
+package com.duffy.gateway.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GatewayConfig {
+
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("api-service-route", r -> r.path("/api/v1/concerts/**")
+                        .uri("lb://api-service"))
+                .build();
+    }
+}
