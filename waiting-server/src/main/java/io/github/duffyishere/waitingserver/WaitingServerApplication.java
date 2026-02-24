4889cc3295d58f9bb1cdad3337c7dd2acff931eb
zzunho9 <jamlove4763@naver.com>
2026-02-24 20:29:08 +0900

    feat: initialize waiting server project

---
+++ waiting-server/src/main/java/io/github/duffyishere/waitingserver/WaitingServerApplication.java
@@ -0,0 +1,13 @@
+package io.github.duffyishere.waitingserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaitingServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WaitingServerApplication.class, args);
+	}
+
+}
---
+++ waiting-server/src/test/java/io/github/duffyishere/waitingserver/WaitingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.duffyishere.waitingserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaitingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
